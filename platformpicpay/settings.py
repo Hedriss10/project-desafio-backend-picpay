96846293bcbfe49acff63b5990cbce7ddc2967d6
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-02 16:29:14 -0300

    requirements: add psycopg

--- platformpicpay/settings.py
+++ platformpicpay/settings.py
@@ -61,10 +61,15 @@ WSGI_APPLICATION = 'platformpicpay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'apirestdjangoframework',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
